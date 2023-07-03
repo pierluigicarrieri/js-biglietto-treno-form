3ec30815831341776bf0d840b5afb1623a7526d0
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-07-03 17:03:46 +0200

    layout done, working on javasctipt

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+const kmFee = 0.21;
+const ageYoung = 18;
+const ageSenior = 65;
+const discountYoung = 20;
+const discountSenior = 40;
+
+let finalDiscount = 0;
+
+
+const usernameInputElement = document.getElementById ("username-input");
+const distanceInputElement = document.getElementById ("distance-input");
+const ageInputElement = document.getElementById ("age-input");
+const buttonGenerate = document.getElementById ("generate");
+
+buttonGenerate.addEventListener ("click", function() 
+
+{
+    const userName = usernameInputElement.value;
+    const distance = distanceInputElement.value;
+    const age = ageInputElement.value;
+}
+)
